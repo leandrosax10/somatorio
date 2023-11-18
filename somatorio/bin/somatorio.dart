5bf1c1e5f7d881d2f394c2de73b97c3e45c8933b
Leandro Santos <44986666+leandrosax10@users.noreply.github.com>
2023-11-18 01:00:51 -0300

    feat: acertos

--- somatorio/bin/somatorio.dart
+++ somatorio/bin/somatorio.dart
@@ -1,17 +1,17 @@
 import 'package:somatorio/utils.dart' as utils;
 
 void main(List<String> arguments) {
-  print("Bem vindo ao Programa Somatorio");
+  print("Bem vindo ao Programa Somatório");
 
-  var numero = utils.lerConsoleInt("Informe um número positivo inteiro -> Ex: 10");
+  var numero =
+      utils.lerConsoleInt("Informe um número positivo inteiro -> Ex: 10");
   calcular(numero);
 }
 
-void calcular( int numero) {
-int resultado = 0;
+void calcular(int numero) {
+  int resultado = 0;
 
-resultado = utils.calculaSomatorio(numero);
-print(" Número informado $numero\n");
-print(" Resultado: $resultado");
+  resultado = utils.calculaSomatorio(numero);
+  print(" Número informado $numero\n");
+  print(" Resultado: $resultado");
 }
-
